9c5e4c8429bf0c3cd20f986df80274be6194643a
Sazonov Daniil <ter6062@ya.ru>
2023-03-01 15:21:50 +0300

    1.1v

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import check.Check;
 import check.CheckMain;
-
-
 public class Main {
     public static void main(String[] args) {
         CheckMain.authorization("java_skypro.go","21431-_4", "21431-_4");
