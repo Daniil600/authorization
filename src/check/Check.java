2dd83f6773cf5d158b28b2add73540d1eb730858
Sazonov Daniil <ter6062@ya.ru>
2023-03-01 15:25:15 +0300

    1.1v

--- src/check/Check.java
+++ src/check/Check.java
@@ -1,43 +1,49 @@
 package check;
+
 import Excecution.WrongLoginException;
 import Excecution.WrongPasswordException;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class Check {
+
     //проверка на использование символов логина
-    public static void checkRightEnterLogin(String login){
+    public static void checkRightEnterLogin(String login) {
         Pattern pt = Pattern.compile("^[aA-zZ(0-9)\\.]*$");
         Matcher mt = pt.matcher(login);
-        if(!mt.matches()){
+        if (!mt.matches()) {
             throw new WrongLoginException();
         }
     }
 
     //проверка на использование символов логина
-    public static void checkRightEnterPassword(String password){
+    public static void checkRightEnterPassword(String password) {
         Pattern pt = Pattern.compile("^[aA-zZ(0-9)\\-\\_]*$");
         Matcher mt = pt.matcher(password);
-        if(!mt.matches()){
+        if (!mt.matches()) {
             throw new WrongLoginException();
         }
     }
+
     //проверка на кол-во символов
-    public static void checkLog(String log){
-        if (log.length() >= 20 ){
+    public static void checkLog(String log) {
+        if (log.length() >= 20) {
             throw new WrongLoginException();
         }
     }
+
     //проверка на кол-во символов
-    public static void checkPass(String password){
-        if(password.length() > 20){
+    public static void checkPass(String password) {
+        if (password.length() > 20) {
             throw new WrongPasswordException();
         }
     }
 
     //проверка на совпадение
-    public static void checkPassConfirm(String password, String passwordConfirm){
-        if(!password.equals(passwordConfirm)){
+    public static void checkPassConfirm(String password, String passwordConfirm) {
+        if (!password.equals(passwordConfirm)) {
             throw new WrongPasswordException();
         }
     }
