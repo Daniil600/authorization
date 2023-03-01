3bcfe67825c48b55f926efbe6736d7c9f4959bd7
Sazonov Daniil <ter6062@ya.ru>
2023-03-01 15:15:55 +0300

    1.1v

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,11 @@
-import Excecution.WrongLoginException;
-import Excecution.WrongPasswordException;
 import check.Check;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Main {
     public static void main(String[] args) {
-        authorization("java_skypro.go","21431-_4", "21431-_4");
-
-
+        Check.authorization("java_skypro.go","21431-_4", "21431-_4");
     }
-
-    public static boolean authorization(String login, String password, String confirmPassword) {
-        try {
-            Check.checkRightEnterLogin(login);
-        }catch (WrongLoginException e){
-            System.out.println("Неверно введён логин\nЛогин содержит в себе только латинские буквы, цифры и знак подчеркивания");
-            return false;
-        }
-        try {
-            Check.checkRightEnterPassword(password);
-        }catch (WrongLoginException e){
-            System.out.println("Неверно введен пароль\nПароль должен содержать в себе только латинские буквы, цифры и знак подчеркивания");
-            return false;
-        }
-        try {
-            Check.checkLog(login);
-        } catch (WrongLoginException e) {
-            System.out.println("login error");
-            return false;
-        }
-        try {
-            Check.checkPass(password);
-        } catch (WrongPasswordException e) {
-            System.out.println("Пароль слошком длинный");
-            return false;
-        }
-        try {
-            Check.checkPassConfirm(password, confirmPassword);
-        } catch (WrongPasswordException e) {
-            System.out.println("Пароли не совпадают");
-            return false;
-        }
-
-        System.out.println("Авторизация прошла успешно!");
-        return true;
-        }
 }
 
 
--- src/check/Check.java
+++ src/check/Check.java
@@ -43,4 +43,40 @@ public class Check {
         }
     }
 
+    public static boolean authorization(String login, String password, String confirmPassword) {
+        try {
+            Check.checkRightEnterLogin(login);
+        }catch (WrongLoginException e){
+            System.out.println("Неверно введён логин\nЛогин содержит в себе только латинские буквы, цифры и знак подчеркивания");
+            return false;
+        }
+        try {
+            Check.checkRightEnterPassword(password);
+        }catch (WrongLoginException e){
+            System.out.println("Неверно введен пароль\nПароль должен содержать в себе только латинские буквы, цифры и знак подчеркивания");
+            return false;
+        }
+        try {
+            Check.checkLog(login);
+        } catch (WrongLoginException e) {
+            System.out.println("login error");
+            return false;
+        }
+        try {
+            Check.checkPass(password);
+        } catch (WrongPasswordException e) {
+            System.out.println("Пароль слошком длинный");
+            return false;
+        }
+        try {
+            Check.checkPassConfirm(password, confirmPassword);
+        } catch (WrongPasswordException e) {
+            System.out.println("Пароли не совпадают");
+            return false;
+        }
+
+        System.out.println("Авторизация прошла успешно!");
+        return true;
+    }
+
 }
