b14c2f64b7ea5cf05b72bbd129ca8cae06dcb2c3
Sazonov Daniil <ter6062@ya.ru>
2023-03-01 15:20:48 +0300

    1.1v

--- src/check/Check.java
+++ src/check/Check.java
@@ -1,8 +1,6 @@
 package check;
-
 import Excecution.WrongLoginException;
 import Excecution.WrongPasswordException;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,41 +41,4 @@ public class Check {
             throw new WrongPasswordException();
         }
     }
-
-    public static boolean authorization(String login, String password, String confirmPassword) {
-        try {
-            Check.checkRightEnterLogin(login);
-        }catch (WrongLoginException e){
-            System.out.println("Неверно введён логин\nЛогин содержит в себе только латинские буквы, цифры и знак подчеркивания");
-            return false;
-        }
-        try {
-            Check.checkRightEnterPassword(password);
-        }catch (WrongLoginException e){
-            System.out.println("Неверно введен пароль\nПароль должен содержать в себе только латинские буквы, цифры и знак подчеркивания");
-            return false;
-        }
-        try {
-            Check.checkLog(login);
-        } catch (WrongLoginException e) {
-            System.out.println("login error");
-            return false;
-        }
-        try {
-            Check.checkPass(password);
-        } catch (WrongPasswordException e) {
-            System.out.println("Пароль слошком длинный");
-            return false;
-        }
-        try {
-            Check.checkPassConfirm(password, confirmPassword);
-        } catch (WrongPasswordException e) {
-            System.out.println("Пароли не совпадают");
-            return false;
-        }
-
-        System.out.println("Авторизация прошла успешно!");
-        return true;
-    }
-
 }
